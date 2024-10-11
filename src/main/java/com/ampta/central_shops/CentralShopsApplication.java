7277053c4d60f9dc9816956c63e4128594a2ce06
ampta <timenakanu@gmail.com>
2024-10-11 13:10:06 +0530

    product, image and category model are added

---
+++ src/main/java/com/ampta/central_shops/CentralShopsApplication.java
@@ -0,0 +1,13 @@
+package com.ampta.central_shops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralShopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralShopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ampta/central_shops/CentralShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ampta.central_shops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
